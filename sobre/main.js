77f92aa07f9acdcb53cf0667a7461264700383d5
PedroCezardev <pcdasilvabeserra@gmail.com>
2024-01-03 23:53:38 -0300

    atualizando aplicação

---
+++ sobre/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+
+    $('.fa-bars').click(function(){
+      $(this).toggleClass('fa-times');
+      $('.navbar').toggleClass('nav-toggle');
+    });
+  
+    $(window).on('load scroll',function(){
+      $('.fa-bars').removeClass('fa-times');
+      $('.navbar').removeClass('nav-toggle');
+  
+      if ($(window).scrollTop() > 30) {
+        $('.header').css({'background': 'rgb(135, 109, 255)', 'box-shadow': '0 .2rem .5rem rgba(0,0,0,.4)'});
+      } else {
+        $('.header').css({'background': 'none', 'box-shadow': 'none'});
+      }
+    });
+  
+  
+    $('.accordion-header').click(function(){
+      $('.accordion .accordion-body').slideUp();
+      $(this).next('.accordion-body').slideDown();
+      $('.accordion .accordion-header span').text('+');
+      $(this).children('span').text('-');
+    });
+  
+  
+  
+  });
+
+  function enviarEmail() {
+    
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+  
+    var body = "Nome: " + firstName + " " + lastName + "\n";
+    body += "Email: " + email + "\n\n";
+    body += "Mensagem: \n" + message;
+  
+    var subject = "Contato via site - Dash Metrics";
+    var mailtoLink = "mailto:pcdasilvabeserra@gmail.com" +
+                     "?subject=" + encodeURIComponent(subject) +
+                     "&body=" + encodeURIComponent(body);
+  
+    window.location.href = mailtoLink;
+  }
