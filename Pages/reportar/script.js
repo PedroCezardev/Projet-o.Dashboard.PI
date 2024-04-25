5a929bf19e3d765401640c05fbd25b4fdf1c9ff2
PedroCezardev <pcdasilvabeserra@gmail.com>
2024-04-24 22:48:54 -0300

    atualizando projeto

--- Pages/reportar/script.js
+++ Pages/reportar/script.js
@@ -1,24 +1,3 @@
-// interface do dashboard
-const sideMenu = document.querySelector('aside');
-const menuBtn = document.getElementById('menu-btn');
-const closeBtn = document.getElementById('close-btn');
-
-const darkMode = document.querySelector('.dark-mode');
-
-menuBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'block';
-});
-
-closeBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'none';
-});
-
-darkMode.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode-variables');
-    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
-    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
-})
-
 // Enviar mensagem
 function sendMessage() {
     var messageInput = document.getElementById('message-input');
